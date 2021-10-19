9c515cc9b846c6a42b21f2a9099fe45ad0885c0d
Ezequiel Becerra <ebecerra1984@gmail.com>
2021-10-19 00:20:16 -0300

    Respaldo

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "views/register.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
 app.listen(3000, () => {
   console.log("el servidor inicio...");
 });
